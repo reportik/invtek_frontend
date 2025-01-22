08cd919970e087d33582ad45e2af3efeb2efa02a
reportik <alberto.jimenez@kener.com.mx>
2025-01-21 22:24:13 -0600

    tarjetas cotizador

--- app/Http/Controllers/dashboard/Analytics.php
+++ app/Http/Controllers/dashboard/Analytics.php
@@ -17,22 +17,22 @@ class Analytics extends Controller
       //throw $th;
     }
     $cards_1 = [
-      ["opcion_radio" => "Muro Interior", "image" => "im1.png"],
-      ["opcion_radio" => "Muro Exterior", "image" => "im2.png"],
-      ["opcion_radio" => "Techo Interior", "image" => "im3.png"],
-      ["opcion_radio" => "Techo Exterior", "image" => "im4.png"],
-      ["opcion_radio" => "Escuadra", "image" => "im5.png"]
+      ["opcion_radio" => "Muro Interior", "image" => "im1.png", "a_selected" => "true"],
+      ["opcion_radio" => "Muro Exterior", "image" => "im2.png", "a_selected" => ""],
+      ["opcion_radio" => "Techo Interior", "image" => "im3.png", "a_selected" => ""],
+      ["opcion_radio" => "Techo Exterior", "image" => "im4.png", "a_selected" => ""],
+      ["opcion_radio" => "Escuadra", "image" => "im5.png", "a_selected" => ""]
     ];
 
     $cards_2 = [
-      ["opcion_radio" => "Tradicional", "image" => "IMG6.jpg"],
-      ["opcion_radio" => "Ripplefold", "image" => "IMG7.jpg"],
-      ["opcion_radio" => "Ojillos", "image" => "IMG8.jpg"]
+      ["opcion_radio" => "Tradicional", "image" => "IMG6.jpg", "a_selected" => "true"],
+      ["opcion_radio" => "Ripplefold", "image" => "IMG7.jpg", "a_selected" => ""],
+      ["opcion_radio" => "Ojillos", "image" => "IMG8.jpg", "a_selected" => ""]
     ];
 
     $cards_3 = [
-      ["opcion_radio" => "Blackout", "image" => "img9.PNG"],
-      ["opcion_radio" => "Sheer", "image" => "img10.PNG"]
+      ["opcion_radio" => "Blackout", "image" => "img9.PNG", "a_selected" => "true"],
+      ["opcion_radio" => "Sheer", "image" => "img10.PNG", "a_selected" => ""]
       // ["opcion_radio" => "Decorativa", "image" => "img11.PNG"]
     ];
 
@@ -42,8 +42,14 @@ class Analytics extends Controller
       ["a_selected" => "false", "title" => "ELIGE EL TIPO DE TELA EN QUE DESEES CONFECCIONAR TU CORTINA", "number" => "3"],
       ["a_selected" => "false", "title" => "ELIGE LA TELA QUE DESEES UTILIZAR", "number" => "4"]
     ];
-
-    return view('main', compact('cards_1', 'cards_2', 'cards_3', 'steps'));
+    $telas = [];
+    try {
+      $response = Http::get('http://localhost:8036/items');
+      $telas = $response->json();
+    } catch (\Throwable $th) {
+      //throw $th;
+    }
+    return view('main', compact('cards_1', 'cards_2', 'cards_3', 'steps', 'telas'));
 
     //return view('welcome');
     // $var = new ComprobacionGastosController();
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts/contentNavbarLayoutOnly' )
 
+<script>
+  function selectEligeTela2(event) {
+  //colocar en tarjeta_imagen el value del select, y en tarjeta_titulo el texto seleccionado del select
+  const selectedValue = event.target.value;
+  const selectedText = event.target.options[event.target.selectedIndex].text;
+  // Colocar en tarjeta_imagen el value del select (base64) con el prefijo adecuado
+  document.getElementById('tarjeta_imagen').src = `data:image/png;base64,${selectedValue}`;
+  // Colocar en tarjeta_titulo el texto seleccionado del select
+  document.getElementById('tarjeta_titulo').innerText = selectedText;
+  }
+</script>
 @section('content')
 
 
@@ -40,7 +51,8 @@
                     <img class="card-img-top" src="{{ asset('images/' . $item['image'])}}" alt="Card image cap">
                     <div class="card-body">
                       <div class="form-check">
-                        <input class="form-check-input" type="radio" name="espacio" value="{{$item['opcion_radio']}}">
+                        <input class="form-check-input" type="radio" name="espacio" value="{{$item['opcion_radio']}}"
+                          @if ($item['a_selected']=='true' ) checked @endif>
                         <label class="form-check-label" for="muroInterior">{{$item['opcion_radio']}}</label>
                       </div>
                     </div>
@@ -51,7 +63,7 @@
 
               <div class="col-12 d-flex justify-content-between">
 
-                <button class="btn btn-primary btn-next waves-effect waves-light"> <span
+                <button style="text-align: right;" class="btn btn-primary btn-next waves-effect waves-light"> <span
                     class="align-middle d-sm-inline-block me-sm-1">Next</span> <i
                     class="ri-arrow-right-line ri-16px"></i></button>
               </div>
@@ -61,8 +73,21 @@
 
           <div id="target_step_2" class="content fv-plugins-bootstrap5 fv-plugins-framework">
             <div class="row g-6">
-              <div class="col-12">
-
+              <div class="row row-cols-1 row-cols-md-3 g-6 mb-6">
+                @foreach ($cards_2 as $item)
+                <div class="col">
+                  <div class="card">
+                    <img class="card-img-top" src="{{ asset('images/' . $item['image'])}}" alt="Card image cap">
+                    <div class="card-body">
+                      <div class="form-check">
+                        <input class="form-check-input" type="radio" name="espacio" value="{{$item['opcion_radio']}}"
+                          @if ($item['a_selected']=='true' ) checked @endif>
+                        <label class="form-check-label" for="muroInterior">{{$item['opcion_radio']}}</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                @endforeach
               </div>
 
               <div class="col-12 d-flex justify-content-between">
@@ -79,7 +104,42 @@
 
           <div id="target_step_3" class="content fv-plugins-bootstrap5 fv-plugins-framework">
             <div class="row g-6">
+              <div class="row row-cols-1 row-cols-md-3 g-6 mb-6">
+                @foreach ($cards_3 as $item)
+                <div class="col">
+                  <div class="card">
+                    <img class="card-img-top" src="{{ asset('images/' . $item['image'])}}" alt="Card image cap">
+                    <div class="card-body">
+                      <div class="form-check">
+                        <input class="form-check-input" type="radio" name="espacio" value="{{$item['opcion_radio']}}"
+                          @if ($item['a_selected']=='true' ) checked @endif>
+                        <label class="form-check-label" for="muroInterior">{{$item['opcion_radio']}}</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                @endforeach
+              </div>
+              <label for="sel_tela_bo" class="form-label">Selecciona tu Tela:</label>
+              <select id="sel_tela_bo" class="form-select form-select-lg" onchange="selectEligeTela(event)">
 
+                @foreach ($telas as $item)
+                <option value="{{ $item['image'] }}">{{ $item['name'] }}</option>
+                @endforeach
+              </select>
+              <!-- Tarjeta -->
+              <div class="card" style="width: 18rem;">
+                @if (count($telas) > 0)
+
+                <img id="tarjeta_imagen" src="{{'data:image/png;base64,'.$telas[0]['image']}}" class="card-img-top"
+                  style="border-radius: 8px 8px 0 0;" alt="Tela Image">
+                <div class="card-body">
+                  <h5 id="tarjeta_titulo" class="card-title">{{$telas[0]['name']}}</h5>
+                  <p class="card-text"></p>
+                </div>
+                @endif
+
+              </div>
               <div class="col-12 d-flex justify-content-between">
                 <button class="btn btn-outline-secondary btn-prev waves-effect"> <i
                     class="ri-arrow-left-line ri-16px me-sm-1 me-0"></i> <span
@@ -94,6 +154,8 @@
 
           <div id="target_step_4" class="content fv-plugins-bootstrap5 fv-plugins-framework">
             <div class="row g-6">
+
+
               <div class="col-12 d-flex justify-content-between">
                 <button class="btn btn-outline-secondary btn-prev waves-effect"> <i
                     class="ri-arrow-left-line ri-16px me-sm-1 me-0"></i> <span
@@ -113,9 +175,9 @@
   <div class="col-md-4">
     <div class="card mt-4">
       <div class="card-body">
-        <h5 class="card-title text-muted fw-bold">Resumen de compra</h5>
+        <h5 class="card-title text-muted fw-bold">Resumen de Cotización</h5>
         <hr>
-        <p class="text-muted small">Aquí verás los importes de tu compra una vez que agregues productos.</p>
+        <p class="text-muted small">Aquí verás los importes.</p>
       </div>
     </div>
   </div>
