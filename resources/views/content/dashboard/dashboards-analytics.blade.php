ecaaa7aa4698007fdea5f867b969f3eabc3e60d8
Iteknia Soporte3 <itk.reportik@gmail.com>
2026-02-04 14:12:38 -0600

    images

--- resources/views/content/dashboard/dashboards-analytics.blade.php
+++ resources/views/content/dashboard/dashboards-analytics.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/contentNavbarLayout')
 
-@section('title', 'Dashboard - KENER')
+@section('title', 'Dashboard - ITEKNIA')
 
 @section('vendor-style')
 @vite('resources/assets/vendor/libs/apex-charts/apex-charts.scss')
